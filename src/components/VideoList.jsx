2d56107d58788bf91052527ab15fd477ceaa37fd
Hack Reactor Student <student@hackreactor.com>
2018-11-26 21:27:55 -0800

    Finish VideoList

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,13 @@
-var VideoList = (props) => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em>{props.videos[0]}</h5></div>
-    <div><h5><em>videoListEntry</em>{props.videos[1]}</h5></div>
-    <div><h5><em>videoListEntry</em>{props.videos[2]}</h5></div>
-    <div><h5><em>videoListEntry</em>{props.videos[3]}</h5></div>
-    <div><h5><em>videoListEntry</em>{props.videos[4]}</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => {
+  var newVids = props.videos.map((video) => <VideoListEntry video={video}/>);
+  return (
+    <div className="video-list">
+      {newVids}
+    </div>
+  )
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
