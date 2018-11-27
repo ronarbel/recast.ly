525d900b4fe4b55745c644cc450f59fef0278d7d
Hack Reactor Student <student@hackreactor.com>
2018-11-26 17:47:35 -0800

    Finish transpilation

---
+++ index.js
@@ -0,0 +1,4 @@
+require('babel-register');
+require('index.js');
+
+console.log('Hello Babel');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 
+ReactDOM.render(<App/>, document.getElementById('app'))
+// require('babel-register');
+// require('index.js');
+
+// console.log('Hello Babel');
